feryw/mt7668-wifi-mod

commit 4ac8b8d73344c0a4f5e16067ea3f36301ad6fa43

[WCNCR00150251] misc: cannot get DHCP ip over VLAN
[Description]
Fix cannot get DHCP ip over VLAN

Due to MT76x8 HW header translation, VLAN pkts would be converted into
80211-LLC VLAN format. But there are IoT issues in customer environment.

In this case, 80211-LLC VLAN format do not work properly. We should play
VLAN pkts as normal ethernet pkts in 80211 MAC translation.

Solution:

Add new capabilities .features in mt66xx_chip_info to detect different
features on different chips.

Tx part
Do not setup VLAN flag in TXD. The header translation would do nothing
and keeps it as pure ethernet payload in 80211 pkts.

Rx part
1. If LLC flag of HTF is setup and this pkt is VLAN type,
do not drop out immediately. Pass up to driver process.
2. Remove "Len" field added by HW header translation

Note:
Please update your FW BINs after commit-id bc897f7(3 Mar 2017)

Feature: misc
Change-Id: Ie395a40557ab5acd1999103a10a86e3ee7e8601f
CR-Id: WCNCR00150251
Signed-off-by: Deren Wu <deren.wu@mediatek.com>

os/linux/gl_kal.c
@@ -852,11 +852,13 @@ WLAN_STATUS kalRxIndicateOnePkt(IN P_GLUE_INFO_T prGlueInfo, IN PVOID pvPkt)
 {
 	struct net_device *prNetDev = prGlueInfo->prDevHandler;
 	struct sk_buff *prSkb = NULL;
+	struct mt66xx_chip_info *prChipInfo;
 
 	ASSERT(prGlueInfo);
 	ASSERT(pvPkt);
 
 	prSkb = pvPkt;
+	prChipInfo = prGlueInfo->prAdapter->chip_info;
 #if DBG && 0
 	do {
 		PUINT_8 pu4Head = (PUINT_8) &prSkb->cb[0];
@@ -930,6 +932,27 @@ WLAN_STATUS kalRxIndicateOnePkt(IN P_GLUE_INFO_T prGlueInfo, IN PVOID pvPkt)
 		       (PUINT_8) prSkb, prSkb->len, prSkb->protocol, prSkb->tail, prSkb->end);
 		DBGLOG_MEM32(RX, ERROR, (PUINT_32) prSkb->data, prSkb->len);
 	}
+
+	if (prSkb->protocol == NTOHS(ETH_P_8021Q)
+		&& !FEAT_SUP_LLC_VLAN_RX(prChipInfo)) {
+		/*
+		*  DA-MAC + SA-MAC + 0x8100 was removed in eth_type_trans()
+		*  pkt format here is TCI(2-bytes) + Len(2-btyes) + payload-type(2-bytes) + payload
+		*  Remove "Len" field inserted by RX VLAN header translation
+		*  Note: TCI+payload-type is a standard 8021Q header
+		*
+		*  This format update is based on RX VLAN HW header translation.
+		*  If the setting was changed, you may need to change rules here as well.
+		*/
+		const UINT_8 vlan_skb_mem_move = 2;
+
+		kalMemCopy(prSkb->data+vlan_skb_mem_move, prSkb->data, vlan_skb_mem_move);
+		skb_pull_rcsum(prSkb, vlan_skb_mem_move);
+		skb_reset_network_header(prSkb);
+		skb_reset_transport_header(prSkb);
+		skb_reset_mac_len(prSkb);
+	}
+
 	if (!in_interrupt())
 		netif_rx_ni(prSkb);	/* only in non-interrupt context */
 	else

chips/mt6632.c
@@ -96,6 +96,7 @@ struct mt66xx_chip_info mt66xx_chip_info_mt6632 = {
 	.patch_addr = MT6632_PATCH_START_ADDR,
 	.is_pcie_32dw_read = MT6632_IS_PCIE_32DW_READ, /* Litien */
 	.eco_info = mt6632_eco_table,
+	.features = 0,
 };
 
 struct mt66xx_hif_driver_data mt66xx_driver_data_mt6632 = {

chips/mt7668.c
@@ -134,6 +134,7 @@ struct mt66xx_chip_info mt66xx_chip_info_mt7668 = {
 	.is_pcie_32dw_read = MT7668_IS_PCIE_32DW_READ, /* Litien */
 	.eco_info = mt7668_eco_table,
 	.constructFirmwarePrio = mt7668ConstructFirmwarePrio,
+	.features = 0,
 };
 
 struct mt66xx_hif_driver_data mt66xx_driver_data_mt7668 = {

common/wlan_lib.c
@@ -4228,9 +4228,11 @@ BOOLEAN wlanProcessTxFrame(IN P_ADAPTER_T prAdapter, IN P_NATIVE_PACKET prPacket
 	UINT_32 u4SysTime;
 	UINT_8 ucMacHeaderLen;
 	TX_PACKET_INFO rTxPacketInfo;
+	struct mt66xx_chip_info *prChipInfo;
 
 	ASSERT(prAdapter);
 	ASSERT(prPacket);
+	prChipInfo = prAdapter->chip_info;
 
 	if (kalQoSFrameClassifierAndPacketInfo(prAdapter->prGlueInfo, prPacket, &rTxPacketInfo)) {
 
@@ -4261,7 +4263,8 @@ BOOLEAN wlanProcessTxFrame(IN P_ADAPTER_T prAdapter, IN P_NATIVE_PACKET prPacket
 			if (rTxPacketInfo.u2Flag & BIT(ENUM_PKT_802_3))
 				GLUE_SET_PKT_FLAG(prPacket, ENUM_PKT_802_3);
 
-			if (rTxPacketInfo.u2Flag & BIT(ENUM_PKT_VLAN_EXIST))
+			if (rTxPacketInfo.u2Flag & BIT(ENUM_PKT_VLAN_EXIST)
+				&& FEAT_SUP_LLC_VLAN_TX(prChipInfo))
 				GLUE_SET_PKT_FLAG(prPacket, ENUM_PKT_VLAN_EXIST);
 
 			if (rTxPacketInfo.u2Flag & BIT(ENUM_PKT_DHCP))

include/nic/mt66xx_reg.h
@@ -913,6 +913,19 @@ typedef struct _ENHANCE_MODE_DATA_STRUCT_T {
 #define WMTCR_TEST_MODE_STATUS          BIT(8)
 #define WMTCR_TEST_MODE_SELECT          BITS(0, 1)
 
+
+
+/* Support features */
+/* Options for VLAN-over-ethernet pkt to/from 802.11 LLC VLAN pkt.
+ * This should depend on the configurations of HW-header-translation.
+ */
+#define FEAT_BITS_LLC_VLAN_TX           BIT(0)
+#define FEAT_BITS_LLC_VLAN_RX           BIT(1)
+
+/* Support features API */
+#define FEAT_SUP_LLC_VLAN_TX(__chip_info) ((__chip_info)->features & FEAT_BITS_LLC_VLAN_TX)
+#define FEAT_SUP_LLC_VLAN_RX(__chip_info) ((__chip_info)->features & FEAT_BITS_LLC_VLAN_RX)
+
 /*******************************************************************************
 *                             D A T A   T Y P E S
 ********************************************************************************
@@ -945,6 +958,8 @@ struct mt66xx_chip_info {
 
 	void (*constructFirmwarePrio)(P_GLUE_INFO_T prGlueInfo, PPUINT_8 apucNameTable,
 	PPUINT_8 apucName, PUINT_8 pucNameIdx, UINT_8 ucMaxNameIdx);/* load firmware bin priority */
+
+	const UINT_32 features;	/* feature bits */
 };
 
 struct mt66xx_hif_driver_data {

nic/nic_rx.c
@@ -1365,6 +1365,7 @@ VOID nicRxProcessDataPacket(IN P_ADAPTER_T prAdapter, IN OUT P_SW_RFB_T prSwRfb)
 	P_SW_RFB_T prRetSwRfb, prNextSwRfb;
 	P_HW_MAC_RX_DESC_T prRxStatus;
 	BOOLEAN fgDrop;
+	struct mt66xx_chip_info *prChipInfo;
 
 	DEBUGFUNC("nicRxProcessDataPacket");
 	/* DBGLOG(INIT, TRACE, ("\n")); */
@@ -1376,6 +1377,7 @@ VOID nicRxProcessDataPacket(IN P_ADAPTER_T prAdapter, IN OUT P_SW_RFB_T prSwRfb)
 
 	prRxStatus = prSwRfb->prRxStatus;
 	prRxCtrl = &prAdapter->rRxCtrl;
+	prChipInfo = prAdapter->chip_info;
 
 	/* Check AMPDU_nERR_Bitmap */
 	prSwRfb->fgDataFrame = TRUE;
@@ -1408,6 +1410,20 @@ VOID nicRxProcessDataPacket(IN P_ADAPTER_T prAdapter, IN OUT P_SW_RFB_T prSwRfb)
 			}
 		}
 #if UNIFIED_MAC_RX_FORMAT
+		else if (HAL_RX_STATUS_IS_LLC_MIS(prRxStatus)
+				&& !HAL_RX_STATUS_IS_ERROR(prRxStatus)
+				&& !FEAT_SUP_LLC_VLAN_RX(prChipInfo)) {
+			PUINT_16 pu2EtherType;
+
+			nicRxFillRFB(prAdapter, prSwRfb);
+
+			pu2EtherType = (PUINT_16)((PUINT_8)prSwRfb->pvHeader + 2*MAC_ADDR_LEN);
+
+			/* If ethernet type is VLAN, do not drop it. Pass up to driver process */
+			if (prSwRfb->u2HeaderLen >= ETH_HLEN
+				&& *pu2EtherType == NTOHS(ETH_P_VLAN))
+				fgDrop = FALSE;
+		}
 #else
 		else if (HAL_RX_STATUS_IS_LLC_MIS(prRxStatus)) {
 			DBGLOG(RSN, EVENT, ("LLC_MIS_ERR\n"));

commit fc800feecc55ad182c1ea65dbb1fc608470afc12

[WCNCR00150960] misc: wrong VLAN ID shown in tcpdump
[Description]
Fix wrong VLAN ID shown in tcpdump

This is a side-effect caused by commit 4ac8b8d73344 ("[WCNCR00150251]
misc: cannot get DHCP ip over VLAN"). There is a 2-bytes hole between
'skb->mac_header + ETH_HLEN' and 'skb->data'. Applications may detect
wrong MAC address if this kind of packets passed up.

To avoid this problem, we have to adjust the memory sequence of ETH-header
properly. Shift whole ETH_HDR(14 bytes) 2 bytes.

Feature: misc
Change-Id: I7db38d26cb9e4b8ea48e5d402c242d85ec01fa5b
CR-Id: WCNCR00150960
Signed-off-by: Deren Wu <deren.wu@mediatek.com>

os/linux/gl_kal.c
@@ -946,8 +946,14 @@ WLAN_STATUS kalRxIndicateOnePkt(IN P_GLUE_INFO_T prGlueInfo, IN PVOID pvPkt)
 		*/
 		const UINT_8 vlan_skb_mem_move = 2;
 
+		/* Remove "Len" and shift data pointer 2 bytes */
 		kalMemCopy(prSkb->data+vlan_skb_mem_move, prSkb->data, vlan_skb_mem_move);
 		skb_pull_rcsum(prSkb, vlan_skb_mem_move);
+
+		/* Have to update MAC header properly. Otherwise, wrong MACs woud be passed up */
+		kalMemMove(prSkb->data - ETH_HLEN, prSkb->data - ETH_HLEN - vlan_skb_mem_move, ETH_HLEN);
+		prSkb->mac_header += vlan_skb_mem_move;
+
 		skb_reset_network_header(prSkb);
 		skb_reset_transport_header(prSkb);
 		skb_reset_mac_len(prSkb);

os/linux/include/gl_kal.h
@@ -579,6 +579,9 @@ typedef struct _MONITOR_RADIOTAP_T {
 /* Zero specific memory block */
 #define kalMemZero(pvAddr, u4Size)                  memset(pvAddr, 0, u4Size)
 
+/* Move memory block with specific size */
+#define kalMemMove(pvDst, pvSrc, u4Size)            memmove(pvDst, pvSrc, u4Size)
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)
 #define strnicmp(s1, s2, n)                         strncasecmp(s1, s2, n)
 #endif