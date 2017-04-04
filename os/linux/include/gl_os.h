feryw/mt7668-wifi-mod

commit 4ea2396438733c91a8813a9d8fde248209ae2b0e

[WCNCR00151687] perf: Improve USB 2.0 peak throughput on TV platform
[Description]
Change to improve USB 2.0 peak throughput on TV platform
- Tune RX aggregation buffer size/number
- Use tasklet for urb complete handling instead of interrupt handler
- Use different spinlocks for different UsbReq queues
- Use usb_anchor_urb related API

Change-Id: I2aa6aa04785efff2465b415baf6558cb111d89d4
Feature: perf
Signed-off-by: Desmond Lin <desmond.lin@mediatek.com>
CR-Id: WCNCR00151687

os/linux/include/gl_os.h
@@ -505,6 +505,8 @@ struct _GLUE_INFO_T {
 	struct task_struct *rx_thread;
 
 #endif
+	struct tasklet_struct rRxTask;
+	struct tasklet_struct rTxCompleteTask;
 
 	struct work_struct rTxMsduFreeWork;
 	struct delayed_work rRxPktDeAggWork;

Makefile.ce
@@ -138,7 +138,7 @@ CFG_DEFAULT_DBG_LEVEL=0xF
 
 CFG_USB_REQ_TX_DATA_CNT=4
 
-CFG_USB_REQ_RX_DATA_CNT=2
+CFG_USB_REQ_RX_DATA_CNT=4
 
 CFG_ENABLE_EFUSE_MAC_ADDR=1
 

include/nic/hal.h
@@ -1048,6 +1048,8 @@ BOOLEAN halIsPendingRx(IN P_ADAPTER_T prAdapter);
 WLAN_STATUS halAllocateIOBuffer(IN P_ADAPTER_T prAdapter);
 WLAN_STATUS halReleaseIOBuffer(IN P_ADAPTER_T prAdapter);
 VOID halDeAggRxPktWorker(struct work_struct *work);
+VOID halRxTasklet(unsigned long data);
+VOID halTxCompleteTasklet(unsigned long data);
 VOID halPrintHifDbgInfo(IN P_ADAPTER_T prAdapter);
 BOOLEAN halIsTxResourceControlEn(IN P_ADAPTER_T prAdapter);
 #endif /* _HAL_H */

include/queue.h
@@ -185,7 +185,7 @@ typedef struct _QUE_T {
 	{ \
 	    ASSERT(prDestQueue); \
 	    ASSERT(prSrcQueue); \
-		if (prSrcQueue->u4NumElem > 0) { \
+		if ((prSrcQueue)->u4NumElem > 0) { \
 			if ((prDestQueue)->prTail) { \
 				((prDestQueue)->prTail)->prNext = (prSrcQueue)->prHead; \
 			} else { \
@@ -201,7 +201,7 @@ typedef struct _QUE_T {
 	{ \
 		ASSERT(prDestQueue); \
 		ASSERT(prSrcQueue); \
-		if (prSrcQueue->u4NumElem > 0) { \
+		if ((prSrcQueue)->u4NumElem > 0) { \
 			((prSrcQueue)->prTail)->prNext = (prDestQueue)->prHead; \
 			(prDestQueue)->prHead = (prSrcQueue)->prHead; \
 			((prDestQueue)->u4NumElem) += ((prSrcQueue)->u4NumElem); \

nic/nic_rx.c
@@ -2883,9 +2883,11 @@ VOID nicRxReturnRFB(IN P_ADAPTER_T prAdapter, IN P_SW_RFB_T prSwRfb)
 	}
 	KAL_RELEASE_SPIN_LOCK(prAdapter, SPIN_LOCK_RX_FREE_QUE);
 
+#if CFG_USB_RX_HANDLE_IN_HIF_THREAD
 	/* Trigger Rx if there are free SwRfb */
 	if (halIsPendingRx(prAdapter) && (prRxCtrl->rFreeSwRfbList.u4NumElem > 0))
 		kalSetIntEvent(prAdapter->prGlueInfo);
+#endif
 }				/* end of nicRxReturnRFB() */
 
 /*----------------------------------------------------------------------------*/

os/linux/gl_init.c
@@ -2236,6 +2236,9 @@ static INT_32 wlanProbe(PVOID pvData, PVOID pvDriverData)
 		prRegInfo->fgEnArpFilter = TRUE;
 #endif
 
+		tasklet_init(&prGlueInfo->rRxTask, halRxTasklet, (unsigned long)prGlueInfo);
+		tasklet_init(&prGlueInfo->rTxCompleteTask, halTxCompleteTasklet, (unsigned long)prGlueInfo);
+
 		if (wlanAdapterStart(prAdapter, prRegInfo) != WLAN_STATUS_SUCCESS)
 			i4Status = -EIO;
 
@@ -2621,6 +2624,9 @@ static VOID wlanRemove(VOID)
 	/* 4 <7> Destroy the device */
 	wlanNetDestroy(prDev->ieee80211_ptr);
 	prDev = NULL;
+
+	tasklet_kill(&prGlueInfo->rTxCompleteTask);
+	tasklet_kill(&prGlueInfo->rRxTask);
 
 	/* 4 <8> Unregister early suspend callback */
 #if CFG_ENABLE_EARLY_SUSPEND

os/linux/hif/pcie/hal_api.c
@@ -1651,6 +1651,16 @@ VOID halDeAggRxPktWorker(struct work_struct *work)
 
 }
 
+VOID halRxTasklet(unsigned long data)
+{
+
+}
+
+VOID halTxCompleteTasklet(unsigned long data)
+{
+
+}
+
 /* Hif power off wifi */
 WLAN_STATUS halHifPowerOffWifi(IN P_ADAPTER_T prAdapter)
 {

os/linux/hif/sdio/hal_api.c
@@ -2017,6 +2017,16 @@ VOID halDeAggRxPkt(P_ADAPTER_T prAdapter, P_SDIO_RX_COALESCING_BUF_T prRxBuf)
 	schedule_delayed_work(&prAdapter->prGlueInfo->rRxPktDeAggWork, 0);
 }
 
+VOID halRxTasklet(unsigned long data)
+{
+
+}
+
+VOID halTxCompleteTasklet(unsigned long data)
+{
+
+}
+
 /* Hif power off wifi */
 WLAN_STATUS halHifPowerOffWifi(IN P_ADAPTER_T prAdapter)
 {

commit 3f8d24f64be68abd330bec1ffd80f853f9289ecf

[WCNCR00151687] perf: Fix build error on 3.10.23-arm
[Description]
Fix build error on 3.10.23-arm, which is introduced from
commit 4ea239643873 ("[WCNCR00151687] perf: Improve USB 2.0 peak
throughput on TV platform").

Change-Id: I7ef641378c01716f8d79051ac8b9c123ef30aa72
Feature: perf
Signed-off-by: Desmond Lin <desmond.lin@mediatek.com>
CR-Id: WCNCR00151687

os/linux/include/gl_os.h
@@ -153,6 +153,7 @@
 #include <linux/firmware.h>	/* for firmware download */
 #include <linux/ctype.h>
 
+#include <linux/interrupt.h>
 
 #if defined(_HIF_USB)
 #include <linux/usb.h>
@@ -162,7 +163,6 @@
 
 #if defined(_HIF_PCIE)
 #include <linux/pci.h>
-#include <linux/interrupt.h>
 #endif
 
 #if defined(_HIF_SDIO)