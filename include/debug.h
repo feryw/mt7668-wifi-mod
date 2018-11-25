450e54610875ee0523a7d5fec812b6b0314bc708
Fabien Parent <fparent@baylibre.com>
2018-11-25 16:11:15 +0100

    debug: disable debug log by default
    It is rather annoying to be spammed all the time with debug log,
    disable the debug log by default to make them go away.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

--- include/debug.h
+++ include/debug.h
@@ -71,7 +71,7 @@
 #define BUILD_QA_DBG 0
 #endif
 
-#define DBG_DISABLE_ALL_LOG             0
+#define DBG_DISABLE_ALL_LOG             1
 
 /*******************************************************************************
 *                    E X T E R N A L   R E F E R E N C E S
@@ -213,9 +213,10 @@ typedef enum _ENUM_DBG_ASSERT_PATH_T {
 *#endif
 */
 #if DBG_DISABLE_ALL_LOG
-#define DBGLOG(_Module, _Class, _Fmt)
+#define DBGLOG(_Module, _Class, _Fmt, ...)
 #define DBGLOG_MEM8(_Module, _Class, _StartAddr, _Length)
 #define DBGLOG_MEM32(_Module, _Class, _StartAddr, _Length)
+#define TOOL_PRINTLOG(_Module, _Class, _Fmt, ...)
 #else
 #define DBGLOG(_Module, _Class, _Fmt, ...) \
 	do { \
